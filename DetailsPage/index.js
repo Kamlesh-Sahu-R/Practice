a3e2a3c1f8cb7ad2f29052ed9626bd3f633bdb64
Kamlesh-Sahu-R <kamleshsahur@gmail.com>
2024-12-02 15:36:13 +0530

    change

--- DetailsPage/index.js
+++ DetailsPage/index.js
@@ -3,12 +3,12 @@ const whatsapp_number = document.getElementById("whatsApp-hide2");
 console.log(whatsapp);
 console.log(whatsapp_number);
 
-whatsapp.addEventListener('mouseover', () => {
+whatsapp.addEventListener('mouseover', function(event){
    
-    if(whatsapp_number.style.display = "none"){
-        whatsapp_number.style.display = "block";
-        whatsapp.style.display = "none";
-    }
+   event.preventDefault();
+    whatsapp_number.style.display = "block";
+    whatsapp.style.display = "none";
+    
    
 });
 whatsapp.addEventListener('mouseout', () => {
