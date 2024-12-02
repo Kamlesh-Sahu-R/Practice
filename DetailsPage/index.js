5ae0cf586e3fd24f6e064dc55fc871a4a1612450
Kamlesh-Sahu-R <kamleshsahur@gmail.com>
2024-12-02 18:36:08 +0530

    change

--- DetailsPage/index.js
+++ DetailsPage/index.js
@@ -1,22 +1,26 @@
 const whatsapp = document.getElementById("whatsApp-hide1");
 const whatsapp_number = document.getElementById("whatsApp-hide2");
-// console.log(whatsapp);
-// console.log(whatsapp_number);
+let count = 1;
 
-// whatsapp_number.style.display = "none";
-
-whatsapp.addEventListener('mouseover', function(event){
-    event.stopPropagation();
-    event.preventDefault();
-    whatsapp_number.style.display = "block";
-    // whatsapp.style.display = "none";
-    
-   
+whatsapp.addEventListener('click', function(event){
+    count += 1;
+    if(count/2){
+        whatsapp_number.style.display = "block";
+        whatsapp.style.display = "none";
+    }else{
+        whatsapp_number.style.display = "none";
+        whatsapp.style.display = "block";
+    }
+   console.log(count);
 });
-whatsapp.addEventListener('mouseout', () => {
-    
-    whatsapp_number.style.display = "none";
-    // whatsapp.style.display = "block";
-    // console.log(whatsapp);
-    // console.log(whatsapp_number);
+whatsapp_number.addEventListener('click', function(event){
+    count += 1;
+    if(count/2){
+        whatsapp_number.style.display = "none";
+        whatsapp.style.display = "block";
+    }else{
+        whatsapp_number.style.display = "block";
+        whatsapp.style.display = "none";
+    }
+   console.log(count);
 });
